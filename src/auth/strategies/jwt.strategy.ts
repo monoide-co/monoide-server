84807239249ec357e2a7222ab5f15c2547683492
KaranP3 <karanpillai93@gmail.com>
2021-08-31 01:24:38 +0530

    Add JWT strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/users/user.entity';
+import { UsersService } from '../../users/users.service';
+import { JwtPayload } from '../jwt-payload.interface';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private usersService: UsersService,
+    private configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload;
+    const user: User = await this.usersService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
