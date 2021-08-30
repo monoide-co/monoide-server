aa965c4a6370f70c954f18695224356fac6ad065
KaranP3 <karanpillai93@gmail.com>
2021-08-31 01:10:13 +0530

    Add initial login implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,34 @@
-import { Body, Post } from '@nestjs/common';
+import { Body, Post, UnauthorizedException } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/login-user.dto';
 import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('register')
-  async register(@Body() registerUserDto: RegisterUserDto): Promise<void> {
+  register(@Body() registerUserDto: RegisterUserDto): Promise<void> {
     return this.usersService.createUser(registerUserDto);
+  }
+
+  @Post('login')
+  async login(
+    @Body() loginUserDto: LoginUserDto,
+  ): Promise<{ accessToken: string }> {
+    const { email } = loginUserDto;
+
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('User does not exist');
+    }
+
+    return this.authService.login(loginUserDto, user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
+  async login(
+    loginUserDto: LoginUserDto,
+    user: User,
+  ): Promise<{ accessToken: string }> {
+    const { email, password } = loginUserDto;
+
+    if (await bcrypt.compare(password, user.password)) {
+      const payload: JwtPayload = { email };
+      const accessToken: string = await this.jwtService.signAsync(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Invalid login credentials');
+    }
+  }
+}
