7bbf0e3164d0efe1c51ed0ead17437936f3ce511
KaranP3 <karanpillai93@gmail.com>
2021-08-30 05:36:06 +0530

    Add user registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
+import { Body, Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('register')
+  async register(@Body() registerUserDto: RegisterUserDto): Promise<void> {
+    return this.usersService.createUser(registerUserDto);
+  }
+}
---
+++ src/auth/dto/register-user.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class RegisterUserDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(32)
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Your password must contain at least one number and have a mixture of uppercase and lowercase letters',
+  })
+  password: string;
+}
